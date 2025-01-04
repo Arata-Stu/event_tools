71f62b30fead84d47edaff460f92f5c7c74d0f25
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2025-01-04 17:05:56 +0900

    update

---
+++ events_info.py
@@ -0,0 +1,18 @@
+import argparse
+from pathlib import Path
+from src.utils.events_reader import H5Reader
+
+def main(h5_file_path: str, camera: str):
+    h5_file_path = Path(h5_file_path)
+    with H5Reader(h5_file_path, camera) as h5r:
+        print(h5r.get_event_summary())
+        print(h5r.time)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert events to images.")
+    parser.add_argument("h5_file_path", type=str, help="Path to the HDF5 file.")
+    parser.add_argument("camera", type=str, help="Camera resolution. Choose from 'VGA' or 'HD'.")
+    args = parser.parse_args()
+
+    main(args.h5_file_path, args.camera)
--- src/utils/events_reader.py
+++ src/utils/events_reader.py
@@ -15,6 +15,7 @@ camera_to_hw = {
     "VGA": (480, 640),
     "HD": (720, 1280),
 }
+
 class H5Reader:
     def __init__(self, h5_file: Path, camera: str):
         assert h5_file.exists(), f"{h5_file} does not exist."
@@ -70,13 +71,16 @@ class H5Reader:
         """
         assert time_array[0] >= 0, "Time must start from non-negative values."
         time_last = 0
-        for idx, time in enumerate(time_array):
-            if time < time_last:
+        for idx, tval in enumerate(time_array):
+            if tval < time_last:
                 time_array[idx] = time_last
             else:
-                time_last = time
+                time_last = tval
 
     def get_event_slice(self, idx_start: int, idx_end: int, convert_2_torch: bool = False) -> dict:
+        """
+        一部分のイベントを取得する。
+        """
         assert self.is_open
         assert idx_end >= idx_start
 
@@ -86,6 +90,7 @@ class H5Reader:
         p_array = np.asarray(ev_data['p'][idx_start:idx_end], dtype='int64')
         t_array = np.asarray(self.time[idx_start:idx_end], dtype='int64')
 
+        # タイムスタンプが昇順か確認
         assert np.all(t_array[:-1] <= t_array[1:])
 
         ev_data = dict(
@@ -97,3 +102,44 @@ class H5Reader:
             width=self.width,
         )
         return ev_data
+
+    def get_event_summary(self) -> dict:
+        """
+        イベントデータ全体の統計情報を返す。
+        - タイムスタンプの最小値・最大値
+        - x, y の最小値・最大値
+        - ON/OFF (p=1, p=0) の数
+        - 全イベント数
+        """
+        assert self.is_open, "File is closed."
+
+        # 全イベントを読み込み (メモリに入る前提)
+        x_array = np.asarray(self.h5f['CD']['events']['x'])
+        y_array = np.asarray(self.h5f['CD']['events']['y'])
+        p_array = np.asarray(self.h5f['CD']['events']['p'])
+        t_array = self.time  # self.timeはすでに補正済みのタイムスタンプを返す
+
+        t_min = t_array.min()
+        t_max = t_array.max()
+        x_min = x_array.min()
+        x_max = x_array.max()
+        y_min = y_array.min()
+        y_max = y_array.max()
+
+        # p=1 を ON イベント数、p=0 を OFF イベント数とみなす例 (データセットに合わせて調整)
+        p_on_count = np.count_nonzero(p_array == 1)
+        p_off_count = np.count_nonzero(p_array == 0)
+        total_count = len(p_array)
+
+        summary = {
+            "t_min": t_min,
+            "t_max": t_max,
+            "x_min": x_min,
+            "x_max": x_max,
+            "y_min": y_min,
+            "y_max": y_max,
+            "p_on_count": p_on_count,
+            "p_off_count": p_off_count,
+            "total_count": total_count
+        }
+        return summary
